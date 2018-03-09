c5bdea91b4b4a5ddc309d52ba6501f70dc3894e0
Warren Chavez <warren@qstrike.com>
2018-03-09 15:03:03 +0800

    Create timeline

--- resources/views/newsfeed.blade.php
+++ resources/views/newsfeed.blade.php
@@ -23,35 +23,48 @@
 </head>
 <body>
 	<div class="uk-container">
-		<form>
-		    <fieldset class="uk-fieldset">
+		<div class="uk-grid-collapse" uk-grid>
+		    <div class="uk-width-1-4@s uk-width-1-4@m">
+		        <!-- <div class="uk-background-primary">Item a</div> -->
+		    </div>
+		    <div class="uk-width-3-4@s uk-width-1-2@m">
+		        <div class="uk-padding-small">
+		        	<form>
+					    <fieldset class="uk-fieldset">
 
-		        <legend class="uk-legend">Post</legend>
+					        <div class="uk-margin">
+					            <textarea class="uk-textarea" rows="3" placeholder="Textarea"></textarea>
+					        </div>
 
-		        <div class="uk-margin">
-		            <input class="uk-input" type="text" placeholder="Title">
+					        <div class="uk-margin">
+					            <input type="submit" class="uk-button uk-button-primary uk-button-small" value="Post">
+					        </div>
+
+					    </fieldset>
+					</form>
+		        	<ul class="uk-list">
+					    <li>
+					    	<article class="uk-comment uk-background-muted uk-padding-small">
+							    <header class="uk-comment-header uk-flex-middle" uk-grid>
+							        <div class="uk-width-expand">
+							            <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top uk-padding-remove">
+							                <li><a href="#">Edit</a></li>
+							                <li><a href="#">Delete</a></li>
+							            </ul>
+							        </div>
+							    </header>
+							    <div class="uk-comment-body">
+							        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+							    </div>
+							</article>
+					    </li>
+					</ul>
 		        </div>
-
-		        <div class="uk-margin">
-		            <textarea class="uk-textarea" rows="5" placeholder="Post Content"></textarea>
-		        </div>
-
-		        <div class="uk-margin">
-		            <input class="uk-input" type="submit" value="Post">
-		        </div>
-
-		    </fieldset>
-		</form>
-
-
-		<dl class="uk-description-list">
-		    <dt>Description term</dt>
-		    <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</dd>
-		    <dt>Description term</dt>
-		    <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</dd>
-		    <dt>Description term</dt>
-		    <dd>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</dd>
-		</dl>
+		    </div>
+		    <div class="uk-width-1-4@s uk-width-1-4@m uk-visible@m">
+		        <!-- <div class="uk-background-secondary">Item c</div> -->
+		    </div>
+		</div>
 	</div>
 </body>
 </html>
